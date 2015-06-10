1ba4dca4bf865a425e020405ed67fb8c9cd816ec
Duccio Giovannelli <giovannelli@extendi.it>
2015-06-10 18:51:19 +0200

    Added tests on json_data_buffer

--- spec/gnip_spec.rb
+++ spec/gnip_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe Gnip do
+  
   it 'has a version number' do
     expect(Gnip::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  
+  it 'should format datetime' do
+    datetime = DateTime.parse("2015-06-01 00:00")
+    expect(Gnip.format_date(datetime)).to eq("201506010000")
   end
+    
 end
