a8080e6a5be23ad02f5a055afed95fb0237070f3
Duccio Giovannelli <giovannelli@extendi.it>
2015-07-08 16:32:59 +0200

    Added backfill_client options to enable backfill on stream

--- lib/gnip/power_track_client.rb
+++ lib/gnip/power_track_client.rb
@@ -3,20 +3,22 @@ module Gnip
       
     attr_accessor :publisher, :label, :account,
                   :username, :password
+                  :backfill_client
       
     attr_reader :rules, :replay_rules, :full_archive, :stream, :replay
       
     def initialize(options = {})
-      @account        = options[:account]
-      @publisher      = options[:publisher]||"twitter"
-      @label          = options[:label]||"dev"
-      @username       = options[:username]
-      @password       = options[:password]
-      @rules          = Gnip::GnipRules::Rules.new(self)
-      @replay_rules   = Gnip::GnipRules::Rules.new(self, true)
-      @full_archive   = Gnip::GnipFullArchive::FullArchive.new(self)
-      @stream         = Gnip::GnipStream::Stream.new(self)
-      @replay         = Gnip::GnipStream::Replay.new(self)
+      @account         = options[:account]
+      @publisher       = options[:publisher]||"twitter"
+      @label           = options[:label]||"dev"
+      @username        = options[:username]
+      @password        = options[:password]
+      @backfill_client = options[:backfill_client]        
+      @rules           = Gnip::GnipRules::Rules.new(self)
+      @replay_rules    = Gnip::GnipRules::Rules.new(self, true)
+      @full_archive    = Gnip::GnipFullArchive::FullArchive.new(self)
+      @stream          = Gnip::GnipStream::Stream.new(self)
+      @replay          = Gnip::GnipStream::Replay.new(self)
     end
       
   end
