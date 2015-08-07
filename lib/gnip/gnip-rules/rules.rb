df0feb065773ca361e97dc289c93065e86f7c8b4
Duccio Giovannelli <giovannelli@extendi.it>
2015-08-07 11:23:31 +0200

    Changed Rules response on add, remove, list, delete_all! and added list_by_tag method

--- lib/gnip/gnip-rules/rules.rb
+++ lib/gnip/gnip-rules/rules.rb
@@ -14,37 +14,86 @@ module Gnip
       #Add rules to PowerTrack rules
       #rules should be an hash in the format {"rules": [{"value": "rule1", "tag": "tag1"}, {"value":"rule2"}]}"
       def add(rules)
-        self.class.post(self.rules_url, basic_auth: @auth, body: rules.to_json)
+        begin
+          response = self.class.post(self.rules_url, basic_auth: @auth, body: rules.to_json)
+          if response.parsed_response["error"].present?
+            { status: :error, code: response.response.code, error: response.parsed_response["error"]["message"] }
+          else
+            { status: :success, code: response.response.code }
+          end
+        rescue Exception => e
+          { status: :error, code: 500, error: e.message }
+        end
+
       end
       
       #Remove rules from PowerTrack rules
       #rules should be an hash in the format {"rules": [{"value": "rule1", "tag": "tag1"}, {"value":"rule2"}]}"
       def remove(rules)
-        self.class.delete(self.rules_url, basic_auth: @auth, body: rules.to_json)
+        begin
+          response = self.class.delete(self.rules_url, basic_auth: @auth, body: rules.to_json)
+          if response.parsed_response["error"].present?
+            { status: :error, code: response.response.code, error: response.parsed_response["error"]["message"] }
+          else
+            { status: :success, code: response.response.code }
+          end
+        rescue Exception => e
+          { status: :error, code: 500, error: e.message }
+        end
+
       end
       
       #Get the full list of rules
       def list
-        self.class.get(self.rules_url, basic_auth: @auth).parsed_response.slice("rules")
+        begin
+          response = self.class.get(self.rules_url, basic_auth: @auth)
+          response.parsed_response.slice("rules").merge({ status: :success, code: response.response.code })
+        rescue Exception => e
+          { status: :error, code: 500, error: e.message }
+        end
+      end
+      
+      #Get the full list of rules by tag
+      def list_by_tag(tag)
+        begin
+          response = self.class.get(self.rules_url, basic_auth: @auth)
+          rules = response.parsed_response["rules"]
+          by_tag_rules = rules.select{|rule| rule["tag"] == tag}
+          return { rules: by_tag_rules }.merge({ status: :success, code: response.response.code })
+        rescue Exception => e
+          { status: :error, code: 500, error: e.message }
+        end
+        
       end
       
       #delete all rules from PowerTrack
       #http://support.gnip.com/apis/powertrack/api_reference.html#DeleteRules
       #Request Body Size Limit 1 MB (~5000 rules)
       def delete_all!
-        rules_list = self.list
-        rules_list["rules"].in_groups_of(2, false).each do |group_of_rules|
-          self.remove({ "rules": group_of_rules })
+        retry_times = 0
+        begin
+          rules_list = self.list
+          rules_list["rules"].in_groups_of(2, false).each do |group_of_rules|
+            self.remove({ "rules": group_of_rules })
+          end
+          sleep 0.05
+          rules_list = self.list
+          if !rules_list["rules"].size.zero?
+            self.delete_all!
+          else
+            return { status: :success, code: 200, rules: []}
+          end
+        rescue Exception => e
+          retry_times += 1
+          if retry_times <= 3
+            retry
+          else
+             { status: :error, code: 500, error: e.message}
+          end 
         end
-        sleep 0.05
-        rules_list = self.list
-        if !rules_list["rules"].size.zero?
-          self.delete_all!
-        else
-          return []
-        end
+
       end
+      
     end
-    
   end
 end
--- lib/gnip/version.rb
+++ lib/gnip/version.rb
@@ -1,3 +1,3 @@
 module Gnip
-  VERSION = "0.1.8"
+  VERSION = "0.1.9"
 end
