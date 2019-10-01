7dda3b34482562760dc5deb154336e44cba8eeb8
Michele Ferri <zomgsako@gmail.com>
2019-10-01 17:27:45 +0200

    fix endpoint url

--- lib/gnip/gnip-full-archive/thirty_day.rb
+++ lib/gnip/gnip-full-archive/thirty_day.rb
@@ -5,8 +5,8 @@ module Gnip
     class ThirtyDay < FullArchive
 
       def initialize(client)
-        @search_url = "https://data-api.twitter.com/search/30day/accounts/#{client.account}/#{client.label}.json"
-        @counts_url = "https://data-api.twitter.com/search/30day/accounts/#{client.account}/#{client.label}/counts.json"
+        @search_url = "https://gnip-api.twitter.com/search/30day/accounts/#{client.account}/#{client.label}.json"
+        @counts_url = "https://gnip-api.twitter.com/search/30day/accounts/#{client.account}/#{client.label}/counts.json"
         @auth = { username: client.username, password: client.password }
       end
 
